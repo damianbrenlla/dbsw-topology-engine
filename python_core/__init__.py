2e253dafa77e0a04581bca28f91a642fbda4ac4e
damianbrenlla <damian.brenlla@dbsw.uk>
2026-08-15 20:27:35 +0100

    Update __init__.py

--- python_core/__init__.py
+++ python_core/__init__.py
@@ -1,8 +1,21 @@
-# DBSW Core Package Initialisation
-# Author: Damian Brenlla / DBSW 2026
+"""
+DBSW R260003 Topology Engine - Core Package Initialization
+Continuum domain discretization, Eurocode material registry, and 3D SIMP solver modules.
 
-from core.domain import Domain3D
-from core.materials import EurocodeMaterialRegistry
-from core.solvers import TopologyOptimiser3DCompliance
+Horizon-2 Service Differentiator | Authorship Engineering Platform
+"""
 
-__all__ = ["Domain3D", "EurocodeMaterialRegistry", "TopologyOptimiser3DCompliance"]
+__version__ = "1.0.0"
+__author__ = "Damian Brenlla / DBSW"
+
+from .domain import Domain3D
+from .materials import MATERIALS, get_material
+from .solvers import run_simp_optimisation, hex8_stiffness_matrix
+
+__all__ = [
+    "Domain3D",
+    "MATERIALS",
+    "get_material",
+    "run_simp_optimisation",
+    "hex8_stiffness_matrix",
+]
